c84cc70a996a5f3c502d85c2c8ca0391052b7cf5
Rodrigo <ropenteadomc@gmail.com>
2023-05-09 17:08:27 -0300

    New Message

--- bgscript2.js
+++ bgscript2.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 // Create a new scene
 const scene2 = new THREE.Scene();
